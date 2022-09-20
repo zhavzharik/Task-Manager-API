494a831c368a0aa2500100743bba787779a3fe4c
Svetlana <zhavzharova2000@yandex.ru>
2022-09-20 03:09:10 +0300

    modified DbInitializer

--- MyTasks.Backend/MyTasks.Persistence/DbInitializer.cs
+++ MyTasks.Backend/MyTasks.Persistence/DbInitializer.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyTasks.Persistence
+﻿namespace MyTasks.Persistence
 {
-    internal class DbInitializer
+    public class DbInitializer
     {
+        public static void Initialize(MyTasksDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
     }
 }
