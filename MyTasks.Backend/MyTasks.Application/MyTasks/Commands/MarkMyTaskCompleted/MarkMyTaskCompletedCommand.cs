01d55c9550a868c5e746119366918e2f6dc32ece
Svetlana <zhavzharova2000@yandex.ru>
2022-09-21 18:23:24 +0300

    add query

--- MyTasks.Backend/MyTasks.Application/MyTasks/Commands/MarkMyTaskCompleted/MarkMyTaskCompletedCommand.cs
+++ MyTasks.Backend/MyTasks.Application/MyTasks/Commands/MarkMyTaskCompleted/MarkMyTaskCompletedCommand.cs
@@ -1,10 +1,6 @@
 ﻿using MediatR;
 using MyTasks.Domain;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyTasks.Application.MyTasks.Commands.MarkMyTaskCompleted
 {
---
+++ MyTasks.Backend/MyTasks.Application/MyTasks/Queries/GetMyTaskDescription/GetMyTaskDescriptionQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using MyTasks.Application.Interfaces;
+using MyTasks.Application.Common.Exceptions;
+using MyTasks.Domain;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyTasks.Application.MyTasks.Queries.GetMyTaskDescription
+{
+    public class GetMyTaskDescriptionQueryHandler
+         : IRequestHandler<GetMyTaskDescriptionQuery, MyTaskDescriptionVm>
+    {
+        private readonly IMyTasksDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetMyTaskDescriptionQueryHandler(IMyTasksDbContext dbContext,
+            IMapper mapper) => (_dbContext, _mapper) = (dbContext, mapper);
+
+        public async Task<MyTaskDescriptionVm> Handle(GetMyTaskDescriptionQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.MyTasks
+                .FirstOrDefaultAsync(mytask =>
+                mytask.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(MyTask), request.Id);
+            }
+
+            return _mapper.Map<MyTaskDescriptionVm>(entity);
+        }
+    }
+}
