fa9917413dc6c3aa0510d2facb6745d7fd979f41
Svetlana <zhavzharova2000@yandex.ru>
2022-09-21 19:05:19 +0300

    dependency injection

---
+++ MyTasks.Backend/MyTasks.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyTasks.Application.Interfaces;
+
+
+namespace MyTasks.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection
+            services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<MyTasksDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IMyTasksDbContext>(provider =>
+                provider.GetService<MyTasksDbContext>());
+            return services;
+        }
+    }
+}
